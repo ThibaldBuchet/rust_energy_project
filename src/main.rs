20f7ce10e6fc5bf5ba516c59de4e4543c2bb3844
Thibald Buchet <thibald.buchet@jobtrek.ch>
2024-06-12 09:07:21 +0200

    Energy project, Read the file.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::fs::File;
+use std::io::prelude::*;
+
+
 fn main() {
-    println!("Hello, rust energy project!");
+  
+  let mut file =File::open("ogd104_stromproduktion_swissgrid.csv").expect("Can't open the file!");
+
+  let mut contents = String::new();
+  file.read_to_string(&mut contents)
+        .expect("Can't read the file!");
+
+    println!("File Contents:\n\n{}", contents);
+
 }

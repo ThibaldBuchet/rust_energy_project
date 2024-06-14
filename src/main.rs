682010ca160de0fc6b1eca75b505a739218e147a
Thibald Buchet <thibald.buchet@jobtrek.ch>
2024-06-14 10:58:59 +0200

    Energy by energy type

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,14 @@ use std::fs::File;
 use std::io::prelude::*;
 use serde_derive::Deserialize;
 use csv::Error;
+use std::collections::HashMap;
 
-fn main() {
-    let mut file = File::open("ogd104_stromproduktion_swissgrid.csv").expect("Can't open the file!");
-
-    let mut contents = String::new();
-    file.read_to_string(&mut contents).expect("Can't read the file!");
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Open the file
+    let file = File::open("ogd104_stromproduktion_swissgrid.csv")?;
+    
+    // CSV reader
+    let mut reader = csv::Reader::from_reader(file);
 
     #[derive(Debug, Deserialize)]
     struct Energy {
@@ -16,14 +18,13 @@ fn main() {
         #[serde(rename = "Energietraeger", default)]
         energy_type: String,
         #[serde(rename = "Produktion_GWh")]
-        production: f64, 
+        production: f64,
     }
 
-    
-    // Reading csv
-    let mut reader = csv::Reader::from_reader(contents.as_bytes());
+    // Vector to store all energy records
     let mut all_energy: Vec<Energy> = vec![];
 
+    // Reading and deserializing CSV records
     for result in reader.deserialize::<Energy>() {
         match result {
             Ok(record) => {
@@ -36,14 +37,26 @@ fn main() {
     }
 
     // Compute total energy production
-    let mut total_production = 0.0;
-    for energy in all_energy.iter() {
-        total_production += energy.production;
+    let total_production: f64 = all_energy.iter().map(|energy| energy.production).sum();
+    println!("Total energy production: {} GWh", total_production);
 
+    // Calculate production by types of energy production
+    let mut production_by_type: HashMap<String, f64> = HashMap::new();
+    for energy in all_energy.iter() {
+        let counter = production_by_type.entry(energy.energy_type.clone()).or_insert(0.0);
+        *counter += energy.production;
     }
 
+    for (energy_type, production) in &production_by_type {
+        println!("{}: {} GWh", energy_type, production);
+    }
+
+    Ok(())
+
+    //Calculate production by years
 
 
 
-    println!("Total energy production: {}", total_production);
+
+
 }

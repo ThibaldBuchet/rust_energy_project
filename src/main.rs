893216a492685ddf8c5f2e398d0fca77abb4f4f6
Thibald Buchet <thibald.buchet@jobtrek.ch>
2024-06-14 09:03:01 +0200

    Compute total energy production

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,49 @@
 use std::fs::File;
 use std::io::prelude::*;
-use std::error::Error;
-use serde::Deserialize;
-
+use serde_derive::Deserialize;
+use csv::Error;
 
 fn main() {
-  
-  let mut file =File::open("ogd104_stromproduktion_swissgrid.csv").expect("Can't open the file!");
+    let mut file = File::open("ogd104_stromproduktion_swissgrid.csv").expect("Can't open the file!");
 
-  let mut contents = String::new();
-  file.read_to_string(&mut contents)
-        .expect("Can't read the file!");
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).expect("Can't read the file!");
 
-    println!("File Contents:\n\n{}", contents);
-
-    struct Energy{
-        date: u64,
+    #[derive(Debug, Deserialize)]
+    struct Energy {
+        #[serde(rename = "Datum", default)]  
+        date: String,
+        #[serde(rename = "Energietraeger", default)]
         energy_type: String,
-        production: f64,
+        #[serde(rename = "Produktion_GWh")]
+        production: f64, 
     }
 
-    impl Energy {
-        fn new(date: u64, energy_type: String, production: f64) -> Self{
-            Self{
-                date,
-                energy_type,
-                production,
+    
+    // Reading csv
+    let mut reader = csv::Reader::from_reader(contents.as_bytes());
+    let mut all_energy: Vec<Energy> = vec![];
+
+    for result in reader.deserialize::<Energy>() {
+        match result {
+            Ok(record) => {
+                all_energy.push(record);
+            },
+            Err(err) => {
+                eprintln!("Error parsing record: {}", err);
             }
         }
-        
-    } 
+    }
 
-    
+    // Compute total energy production
+    let mut total_production = 0.0;
+    for energy in all_energy.iter() {
+        total_production += energy.production;
 
+    }
+
+
+
+
+    println!("Total energy production: {}", total_production);
 }
